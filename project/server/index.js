28c775edbeb857351e64f213c0dae5b78f18a7ab
vladimir <vlprosvirin@gmail.com>
2023-10-02 22:00:01 +0300

    my first com

---
+++ project/server/index.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const express=require('express')
+const cors=require('cors')
+const cookieParser=require('cookie-parser')
+const mongoose=require('mongoose')
+const app =express()
+const PORT=process.env.PORT || 5000
+const router =require('./router/index')
+const erromiddlewear=require('./middlewear/error-middlewear')
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use('/auth',router)
+app.use(erromiddlewear)
+
+
+const start= async ()=>{
+    try{
+        await mongoose.connect(process.env.DB_URL,{
+
+            useUnifiedTopology:true
+        })
+    app.listen(PORT,()=>{
+        console.log('server starts on '+PORT)
+    })
+    }catch(e){
+        console.log(e)
+    }
+}
+start()
